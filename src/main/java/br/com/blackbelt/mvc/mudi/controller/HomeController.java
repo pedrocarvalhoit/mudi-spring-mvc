fcf476c8d9e901ddf1fb51c4a5e43449d44f9f4e
Pedro Duarte <pedrohdc@yahoo.com.br>
2022-10-05 23:12:18 +0100

    HomeC

--- src/main/java/br/com/blackbelt/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/blackbelt/mvc/mudi/controller/HomeController.java
@@ -3,6 +3,7 @@ package br.com.blackbelt.mvc.mudi.controller;
 import br.com.blackbelt.mvc.mudi.model.Pedido;
 import br.com.blackbelt.mvc.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
-public class HomeController {
+public class HomeController{
 
     @Autowired //Utiliza sem precisar instanciar
-    PedidoRepository repository;
+    PedidoRepository pedidoRepository;
 
     @GetMapping("/home")
     public String home(Model model){
-        List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
-        model.addAttribute("pedidos", pedidos);
-        return "home";
+        List<Pedido> pedidos= pedidoRepository.findAll();
+        model.addAttribute("pedidos",pedidos);
+        return"home";
     }
 
 }

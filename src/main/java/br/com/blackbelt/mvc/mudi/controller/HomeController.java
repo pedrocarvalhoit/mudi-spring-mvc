3c4447f9a71c7c03c3e5c14fcd3225bc4b898398
Pedro Duarte <pedrohdc@yahoo.com.br>
2022-11-09 11:14:05 +0000

    blackbelt store

--- src/main/java/br/com/blackbelt/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/blackbelt/mvc/mudi/controller/HomeController.java
@@ -4,6 +4,9 @@ import br.com.blackbelt.mvc.mudi.model.Produto;
 import br.com.blackbelt.mvc.mudi.model.StatusProduto;
 import br.com.blackbelt.mvc.mudi.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,14 +28,19 @@ public class HomeController{
 
     @GetMapping
     public String home(Model model, Principal principal){ //Principal ingeta os dados do usuário logado
-        List<Produto> produtos = produtoRepository.findAll();//Lista os pedidos por usuário
+        Sort sort = Sort.by("nomeProduto").ascending();
+        PageRequest page = PageRequest.of(0,10, sort);
+        Page<Produto> produtos = produtoRepository.findAll(page);//Lista os pedidos por usuário
         model.addAttribute("produtos", produtos);
         return"home";
     }
 
     @GetMapping("/{status}")//Garante o acesso pela digitação de qualquer status
     public String porStatus(@PathVariable("status") String status, Model model){
-        List<Produto> produtos = produtoRepository.findByStatus(StatusProduto.valueOf(status.toUpperCase()));
+        Sort sort = Sort.by("valorNegociado").ascending(); //Escolha da ordenação
+        PageRequest page = PageRequest.of(0, 10, sort);//Paginação-incluindo o SORT
+
+        List<Produto> produtos = produtoRepository.findByStatus(StatusProduto.valueOf(status.toUpperCase()), page);
         model.addAttribute("produtos", produtos);
         model.addAttribute("status", status);//Acessa o valor do Estado do Pedido
         return"home";
--- src/main/java/br/com/blackbelt/mvc/mudi/repository/ProdutoRepository.java
+++ src/main/java/br/com/blackbelt/mvc/mudi/repository/ProdutoRepository.java
@@ -2,6 +2,8 @@ package br.com.blackbelt.mvc.mudi.repository;
 
 import br.com.blackbelt.mvc.mudi.model.Produto;
 import br.com.blackbelt.mvc.mudi.model.StatusProduto;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,7 +15,12 @@ import java.util.List;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
-    List<Produto> findByStatus(StatusProduto status);
+    @Cacheable("test2")
+    List<Produto> findAll();
+
+    //Pageable cuida da ordenação das páginas -> @EnableCaching na application
+    @Cacheable("test")
+    List<Produto> findByStatus(StatusProduto status, Pageable sort);
 
     @Query("SELECT p from Produto p join p.user u where u.username = :username")
     //Param mapeia o username solicitado no método para a query
